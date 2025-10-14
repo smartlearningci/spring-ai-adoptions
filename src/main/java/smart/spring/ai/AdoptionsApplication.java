c221dd2288e4d0ca55409ea5cb72434fce6e7d2f
eduardo <eduardo@smart-learning.pt>
2025-10-14 11:31:29 +0100

    chore: initial commit

---
+++ src/main/java/smart/spring/ai/AdoptionsApplication.java
@@ -0,0 +1,13 @@
+package smart.spring.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/smart/spring/ai/AdoptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package smart.spring.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
