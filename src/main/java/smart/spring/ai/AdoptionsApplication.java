9364af6c0515b98dbe5592a508600ee6e01577b3
eduardo <eduardo@smart-learning.pt>
2025-10-16 11:51:08 +0100

    update postgres e ligação ollama

--- src/main/java/smart/spring/ai/AdoptionsApplication.java
+++ src/main/java/smart/spring/ai/AdoptionsApplication.java
@@ -1,14 +1,29 @@
 package smart.spring.ai;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ai.chat.client.ChatClient;
 
-//Teste aos comentários
 @SpringBootApplication
 public class AdoptionsApplication {
+  public static void main(String[] args) { SpringApplication.run(AdoptionsApplication.class, args); }
+}
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdoptionsApplication.class, args);
-	}
+@Controller
+@ResponseBody
+class AdoptionsController {
 
+  private final ChatClient ai;
+
+  AdoptionsController(ChatClient.Builder ai) {
+    this.ai = ai.build();
+  }
+
+  @GetMapping("/{user}/assistant")
+  String ask(@PathVariable String user, @RequestParam String question) {
+    return ai.prompt().user(question).call().content();
+  }
 }
